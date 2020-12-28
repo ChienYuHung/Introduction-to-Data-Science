62e862fbd18f23e74feef52f6843c108f864c9ef
簡郁虹 <jessica092090@gmail.com>
2020-12-28 14:24:01 +0800

    Add files via upload

---
+++ week-16-74.py
@@ -0,0 +1,40 @@
+print(True)
+print(False)
+
+print(bool(1))
+print(bool(0))
+print(bool(()))
+
+print(False + 1)
+score == 0
+score == 66
+score == 66
+
+name = "Francis"
+name = "Francis"
+
+name = "Francis"
+
+5>2
+5,2
+5 !=2
+
+True & True
+True & False
+False & True
+False & False
+
+True or True
+True or False
+False or True
+False or False
+
+not True
+not False
+
+X = 70
+print((X > 60) and (X < 80))
+print((X > 60) and (X < 80))
+
+"H"in"Hello world"
+"h"in "Hello world"
